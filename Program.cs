efe8a8541e214394833244a3e0b0e99843079ca2
北海若 <shiruoxi2010@163.com>
2019-03-10 09:46:39 +0800

    Managed-Native Layer Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace th902_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
